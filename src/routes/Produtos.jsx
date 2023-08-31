ac710c3b774e5d87676c464b88480616fea3b115
guinaoki <rm551456@fiap.com.br>
2023-08-31 11:06:09 -0300

    Listagem dos produtos em tabela realizada.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,39 @@
+import { ListaProdutos } from "../components/ListaProdutos"
+
 export default function produtos(){
     return(
         <>
-        <h1>Produtos</h1>
+        <h1>Produtos Informáticos - FIAPO</h1>
+
+        <table>
+
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        {ListaProdutos.map((produto,indice)=>(
+            <tr key={indice}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.desc}</td>
+                <td>{produto.preco}</td>
+            </tr>
+        ))}
+        </tbody>
+
+        <tfoot>
+            <tr>
+                <td colSpan={3}>PRODUTOS</td>
+            </tr>
+        </tfoot>
+
+        </table>
         </>
     )
 }

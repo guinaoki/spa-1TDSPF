e28def88549891cf19399fcfc56b5b6fdd82d23e
Guilherme Naoki Hirayama <126685457+guinaoki@users.noreply.github.com>
2023-08-31 09:09:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
+import { Outlet } from "react-router-dom";
+
+export default function App() {
+
+  return (
+    <>
+      <Cabecalho />
+
+      <Outlet/>
+
+      <Rodape />
+    </>
+  );
+}
---
+++ src/routes/Erro.jsx
@@ -0,0 +1,7 @@
+export default function erro(){
+    return(
+        <>
+        <h1>Erro 404</h1>
+        </>
+    )
+}

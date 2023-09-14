fec48e2800b58fb75dda8cfa68553cb7b4406cb4
guinaoki <rm551456@fiap.com.br>
2023-09-14 10:55:00 -0300

    Produto excluir criado.

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { ListaProdutos } from '../components/ListaProdutos';
 
 export default function ExcluirProdutos() {
+
+  document.title = 'EXCLUIR PRODUTO'
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+
   return (
     <>
         <div>
           <h1>Excluir Produtos</h1>
           
           <div>
+            <h2>Produto Selecionado</h2>
             <figure>
-              <img src="" alt="" />
-              <figcaption>Nome - Preço</figcaption>
+              <img src={produto.img} alt={produto.desc} />
+              <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
             </figure>
           </div>
 

5a0caba97c1e1c536dea20e2c49bd96964a7994c
guinaoki <rm551456@fiap.com.br>
2023-09-27 12:16:08 -0300

    Novo Id registrado no console

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -2,6 +2,27 @@ import styles from "./ModalInserir.module.css";
 
 export default function ModalInserir(){
 
+    document.title = "CADASTRO"/node_modules
+
+    let novoId;
+
+    fetch("http://localhost:5000/produtos",{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        }
+        
+    })
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log(data)
+        return novoId = (data[data.length-1].id+1)
+    })
+    .catch(error=> console.log(error));
+
+    console.log("NOVO ID " + novoId)
+    return novoId
+
 if (props.open){
     return(
         <div className={styles.container}>

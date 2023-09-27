367e2346c3f5a35cdb8221d7fac4da28e4118a08
guinaoki <rm551456@fiap.com.br>
2023-09-27 11:54:36 -0300

    Iniciando a construção do ModalInserir.

---
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -0,0 +1,13 @@
+import styles from "./ModalInserir.module.css";
+
+export default function ModalInserir(){
+
+if (props.open){
+    return(
+        <div className={styles.container}>
+            <h1>Modal Inserir</h1>
+            <button onClick={()=>props.setOpen(false)}>CLOSE-MODAL</button>
+        </div>
+    )
+    }
+}

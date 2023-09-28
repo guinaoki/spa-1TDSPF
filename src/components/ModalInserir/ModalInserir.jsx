0b0229e307b62c8b43eef0335961cb10f53cf21d
guinaoki <rm551456@fiap.com.br>
2023-09-28 12:19:38 -0300

    Arrumando o SASS

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -17,51 +17,45 @@ export default function ModalInserir(props) {
     .then((data)=>{
         console.log(data)
         novoId = (data[data.length-1].id+1)
-        console.log("NOVO ID " + novoId);
         return novoId;
-        
         })
     .catch(error=> console.log(error));
 
-if (props.open){
-    return(
-        <div className={styles.container}>
-            <h1>CADASTRO DE PRODUTOS</h1>
 
+ if (props.open) {
+    return (
+      <div className={styles.container}>
+        <h1>CADASTRO DE PRODUTOS</h1>
 
-            <div className="formGroup">
-                <form>
-                    <fieldset>
-                    <span className="btnClose" onClick={()=>props.setClose(false)}>X</span>
-                        <legend>Novo Produto</legend>
-                        <div>
-                            <label htmlFor="idNome">Nome</label>
-                            <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto." />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="idDesc">Deacrição</label>
-                            <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto." />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="idPreco">Preço</label>
-                            <input type="text" name="preco" id="idPreco" placeholder="Digite o valor do produto." />
-                        </div>
-                        
-                        <div>
-                            <label htmlFor="idImg">Imagem</label>
-                            <input type="url" name="img" id="idImg" placeholder="Digite a url da imagem do produto." />
-                        </div>
-                        <div>
-                            <button>CADASTRAR</button>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-
-
+        <div className="formGroup">
+          <form>
+              <fieldset>
+        <span className="btnClose" onClick={()=> props.setOpen(false)}>X</span>
+                <legend>Novo Produto</legend>
+                <div>
+                  <label htmlFor="idNome">Nome</label>
+                  <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto."/>
+                </div>
+                <div>
+                  <label htmlFor="idDesc">Descrição</label>
+                  <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto."/>
+                </div>
+                <div>
+                  <label htmlFor="idPreco">Preço</label>
+                  <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto."/>
+                </div>                
+                <div>
+                  <label htmlFor="idImg">Imagem</label>
+                  <input type="url" name="img" id="idImg" placeholder="Digite a url da imagem do produto."/>
+                </div>                
+                <div>
+                  <button>CADASTRAR</button>
+                </div>                
+              </fieldset>
+          </form>
         </div>
+
+      </div>
     );
-    }
+  }
 }

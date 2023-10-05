b2a0adb49655fdeb9b780bb550c117d80dfef011
guinaoki <rm551456@fiap.com.br>
2023-10-05 11:38:27 -0300

    Estilizção do cabecalho com SASS e condicional ternário.

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,21 @@
+import { Link, useLocation } from "react-router-dom";
+import "./Cabecalho.scss";
+
+export default function Cabecalho(){
+
+    const rotaAtual = useLocation();
+    console.log("ROTA ATUAL : " + rotaAtual.pathname)
+
+    return(
+        <>
+            <header className="cabecalho">
+                <nav>
+                    <ul>
+                        <li><Link to="/" className={ rotaAtual.pathname == "/" ? "active" : ""}>Home</Link></li>
+                        <li><Link to="/produtos" className={ rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
